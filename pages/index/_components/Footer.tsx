9ba0bd9133cfad73eef5070bd873f0a40ab348e5
blackmanba <wen578351314@gmail.com>
2023-03-31 13:49:04 +0800

    备案号

--- pages/index/_components/Footer.tsx
+++ pages/index/_components/Footer.tsx
@@ -2,7 +2,7 @@ export default () => {
   return (
     <footer mt-12>
       <p text-sm op-40>
-        Blackmanba © 2023
+        Blackmanba © 2023 <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备19028629号-1</a>
       </p>
     </footer>
   )
